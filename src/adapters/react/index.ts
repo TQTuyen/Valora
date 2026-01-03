e5413da051b299e59a7542c751a28822a4a47e31
Le Thanh Vinh <thanhvinh2400@gmail.com>
2026-01-03 10:06:50 +0700

    feat: implement reactive hooks inside adapter class

--- src/adapters/react/index.ts
+++ src/adapters/react/index.ts
@@ -42,4 +42,93 @@ export class ReactAdapter<T extends Record<string, unknown>> extends BaseFramewo
     super(validators, options);
   }
 
-  
+  useField<K extends keyof T>(field: K): ReactFieldState<T[K]> {
+    const [fieldState, setFieldState] = useState<FieldState<T[K]>>(() => {
+      const state = this.getFieldState(field);
+      return (
+        state ?? {
+          value: undefined,
+          touched: false,
+          dirty: false,
+          validating: false,
+          errors: [],
+          isValid: true,
+        }
+      );
+    });
+
+    useEffect(() => {
+      const unsubscribe = this.subscribeToField(field, (state: FieldState<T[K]>) => {
+        setFieldState({ ...state });
+      });
+
+      return () => {
+        unsubscribe();
+      };
+    }, [field]);
+
+    // Memoize computed properties
+    const hasError = useMemo(() => hasFieldErrors(fieldState), [fieldState.errors]);
+    const firstError = useMemo(() => getFirstError(fieldState), [fieldState.errors]);
+    const shouldShowError = useMemo(
+      () => shouldShowErrors(fieldState),
+      [fieldState.touched, fieldState.errors],
+    );
+    const errorMessages = useMemo(() => formatErrors(fieldState), [fieldState.errors]);
+
+    return {
+      ...fieldState,
+      hasError,
+      firstError,
+      shouldShowError,
+      errorMessages,
+    };
+  }
+
+
+  useForm(): ReactFormState<T> {
+    const [formState, setFormState] = useState<FormState<T>>(() => this.getFormState());
+
+    useEffect(() => {
+      const unsubscribe = this.subscribeToForm((state: FormState<T>) => {
+        setFormState({ ...state });
+      });
+
+      return () => {
+        unsubscribe();
+      };
+    }, []);
+
+    const canSubmitValue = useMemo(() => canSubmit(formState), [formState.isValid, formState.validating]);
+
+    return {
+      ...formState,
+      canSubmit: canSubmitValue,
+    };
+  }
+
+
+  getFieldBindings<K extends keyof T>(field: K) {
+    const fieldState = this.getFieldState(field);
+
+    return {
+      value: fieldState?.value ?? ('' as T[K]),
+      onChange: (value: T[K]) => {
+        this.setFieldValue(field, value);
+      },
+      onBlur: () => {
+        this.touchField(field);
+      },
+      error: getFirstError(fieldState),
+      hasError: hasFieldErrors(fieldState),
+      shouldShowError: shouldShowErrors(fieldState),
+      errorMessages: formatErrors(fieldState),
+    };
+  }
+}
+
+
+
+
+
+export type { ValidatorMap, FormStateOptions };
